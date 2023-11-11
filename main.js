e7ac62e4474b0bda0df9769f62197b5939323c02
Marcin Czekajski <czekajskimarcin@gmail.com>
2023-11-11 22:31:51 +0100

    fullscreen

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import "./style.css";
 
 //Scene
 const scene = new THREE.Scene();
@@ -11,19 +12,43 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+//Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
 //Light
 const light = new THREE.PointLight(0xfffff, 200, 100);
 light.position.set(0, 10, 10);
 scene.add(light);
 
 //Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600);
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
 camera.position.z = 10;
 scene.add(camera);
 
 //Renderer
 const canvas = document.querySelector(".webgl");
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+renderer.setSize(sizes.width, sizes.height);
 
 renderer.render(scene, camera);
+
+// Resize
+window.addEventListener("resize", () => {
+  //Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  //Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const loop = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+loop();
